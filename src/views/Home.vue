ecac6fd379397648fc3e3e08880a8c40f2ca91bd
Kotus <bastien35@gmail.com>
2020-11-09 19:50:42 +0100

    ✨ Adding new supported colors and refactoring scss folders

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,23 +9,35 @@
       <el-button class="el-button--primary el-button--hoverable" @click="drawer = !drawer">
         Open drawer
       </el-button>
-      <el-button class="el-button--primary" @click="drawer = !drawer">
+      <el-button class="el-button--success" @click="drawer = !drawer">
         Open drawer
       </el-button>
-      <el-button class="el-button--secondary el-button--translatable" @click="drawer = !drawer">
+      <el-button class="el-button--danger el-button--translatable" @click="drawer = !drawer">
         Open drawer
       </el-button>
-      <el-button class="el-button--secondary el-button--hoverable el-button--translatable" @click="drawer = !drawer">
+      <el-button class="el-button--warning el-button--hoverable el-button--translatable" @click="drawer = !drawer">
+        Open drawer
+      </el-button>
+      <el-button disabled class="el-button--secondary el-button--hoverable el-button--translatable el-button--focusable" @click="drawer = !drawer">
         Open drawer
       </el-button>
       <el-drawer id="admin-drawer" :visible.sync="drawer" :with-header="false">
-        <div class="el-drawer__content">
+        <div class="el-drawer__content flex flex-column">
           <header class="flex align-start justify-between mb-4">
             <h4 class="heading heading--bold">Titre</h4>
             <div>
               <font-awesome-icon class="cursor-pointer size-3 hover-y-translate" @click="drawer = !drawer" icon="times" role="img"/>
             </div>
           </header>
+          <main class="self-grow">
+            Content
+          </main>
+          <footer>
+            <el-button @click="drawer = !drawer" class="el-button--white el-button--translatable el-button--bold el-button--full el-button--uppercase" style="--text-color:var(--primary-color)">
+              <font-awesome-icon icon="check-circle" class="text-success" />
+              Save the data
+            </el-button>
+          </footer>
         </div>
       </el-drawer>
     </div>
